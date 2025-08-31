cd60d28cfb0009ce517bfa86d5f8592f3e10c6f6
Joseph Ding <dingyiheng91@gmail.com>
2025-08-31 19:27:16 +0800

    init: first init

---
+++ src/main/java/com/example/todolist_practice/TodolistPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistPracticeApplication.class, args);
+	}
+
+}
